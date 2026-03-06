c60d56911947470aaeda824051d4aff74131cb34
Jetse <jetsemani81@gmail.com>
2026-03-06 17:24:08 +0100

    differents-injected-dependencies

--- spring-6-di/src/main/java/group/springframework/spring_6_di/controllers/ConstructorInjectedController.java
+++ spring-6-di/src/main/java/group/springframework/spring_6_di/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package group.springframework.spring_6_di.controllers;
 
 import group.springframework.spring_6_di.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
--- spring-6-di/src/main/java/group/springframework/spring_6_di/controllers/SetterInjectedController.java
+++ spring-6-di/src/main/java/group/springframework/spring_6_di/controllers/SetterInjectedController.java
@@ -1,15 +1,24 @@
 package group.springframework.spring_6_di.controllers;
 
 import group.springframework.spring_6_di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
+
+
     private GreetingService greetingService;
 
-    public void setGreetingService(GreetingService greetingService) {
+    @Autowired
+    public void setGreetingService(GreetingService greetingService)
+    {
+        System.out.println("###############Setter ejecutado###############");
         this.greetingService = greetingService;
     }
 
     public String sayHello() {
+        System.out.println("Say Hello ejecutado");
         return greetingService.sayGreeting();
     }
 }
