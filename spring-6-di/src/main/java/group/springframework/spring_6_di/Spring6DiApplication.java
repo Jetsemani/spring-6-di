298e5b561d6cd13197ec545217e27d737a73b41c
Jetse <jetsemani81@gmail.com>
2026-03-06 13:42:06 +0100

    initial commit

---
+++ spring-6-di/src/main/java/group/springframework/spring_6_di/Spring6DiApplication.java
@@ -0,0 +1,18 @@
+package group.springframework.spring_6_di;
+
+import group.springframework.spring_6_di.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Spring6DiApplication.class, args);
+
+
+	}
+
+}
