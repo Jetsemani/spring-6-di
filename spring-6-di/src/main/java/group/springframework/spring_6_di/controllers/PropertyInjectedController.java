bc2715492bebbb4b75612f2366201e6685d1948a
Jetse <jetsemani81@gmail.com>
2026-03-06 16:23:15 +0100

    differents-injected-controllers

---
+++ spring-6-di/src/main/java/group/springframework/spring_6_di/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package group.springframework.spring_6_di.controllers;
+
+import group.springframework.spring_6_di.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    GreetingService greetingService;
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
